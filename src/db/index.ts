738b2e50b5d94d6ea4636e0bb0c20f8794d0da6d
Ken_Lee <493129720@qq.com>
2025-05-28 00:06:06 +0800

    feat: 添加数据库表

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,3 +5,5 @@ async function main() {
 }
 
 main();
+
+// chat
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,18 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const chatsTable = pgTable("chat", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull(),
+  title: text("text").notNull(),
+  model: text("model").notNull(),
 });
+
+export const messagesTable = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("chat_id").references(() => chatsTable.id),
+  role: text("role").notNull(),
+  content: text("content").notNull(),
+});
+
+export type ChatModel = typeof chatsTable.$inferSelect;
+export type MessagesModel = typeof chatsTable.$inferSelect;
