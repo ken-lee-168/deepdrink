9d6759dc70501efb4d9c0e19f555a15a0de53f96
Ken_Lee <493129720@qq.com>
2025-05-29 00:23:29 +0800

    feat：navbar接口联调；

--- src/app/components/Navibar.tsx
+++ src/app/components/Navibar.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import React from "react";
 type Props = {};
 interface NavibarItem {
@@ -6,15 +11,35 @@ interface NavibarItem {
 
 function Navibar({}: Props) {
   const [navibarList, setNavibarList] = React.useState<NavibarItem[]>([]);
+  const { user } = useUser();
+  const router = useRouter();
 
-  const getNavibarList = () => {
+  const { data } = useQuery({
+    queryKey: ["chats"],
+  });
 
-  }
+  const getNavibarList = () => {};
 
   return (
-    <div>
-      navibar
-      <div>
+    <div className="h-screen bg-gray-200">
+      <div className="flex items-center justify-center">
+        <p className="font-bold text-2xl">deepdrink</p>
+      </div>
+
+      <div
+        className="h-10 flex items-center justify-center mt-4 cursor-pointer"
+        onClick={() => {
+          console.log("router");
+          router.push("/");
+        }}
+      >
+        <p className="h-full w-2/3 bg-blue-300 rounder-lg flex items-center justify-center font-thin text-blue-700">
+          开启新对话
+        </p>
+      </div>
+
+      {/* 目录 */}
+      <div className="flex flex-col items-center justify-center gap-2 p-6">
         {navibarList.map((item, index) => (
           <div key={index}>{item.key}</div>
         ))}
