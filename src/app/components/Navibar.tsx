08028263246db7bc71442b5ad9ac9d6704695cd7
lijianwei <lijianwei@dianxiaomi.com>
2025-05-28 20:30:20 +0800

    feat: navibar左侧菜单栏；

--- src/app/components/Navibar.tsx
+++ src/app/components/Navibar.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-
 type Props = {};
-
-function navibar({}: Props) {
-  return <div>navibar</div>;
+interface NavibarItem {
+  key: string;
 }
 
-export default navibar;
+function Navibar({}: Props) {
+  const [navibarList, setNavibarList] = React.useState<NavibarItem[]>([]);
+
+  const getNavibarList = () => {
+
+  }
+
+  return (
+    <div>
+      navibar
+      <div>
+        {navibarList.map((item, index) => (
+          <div key={index}>{item.key}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Navibar;
